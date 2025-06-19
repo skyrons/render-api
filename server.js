940a193144d76e74e403ab6087357203ab43e8c4
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2025-06-19 14:36:06 -0300

    Usando server.js compatível com PORT para Render

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 4000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`JSON Server is running at http://localhost:${port}`);
+});
